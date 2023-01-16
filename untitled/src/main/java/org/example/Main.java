797c736fe09d003b4f405f5ac26486393c1ad1d0
goran <gorankn89@gmail.com>
2023-01-16 04:22:57 +0100

    Final Commit Asci Generator :)

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        ASCIIArtGenerator artGen = new ASCIIArtGenerator();
+
+        System.out.println();
+        artGen.printTextArt("Hello!", ASCIIArtGenerator.ART_SIZE_SMALL);
+        System.out.println();
+
+        System.out.println();
+        artGen.printTextArt("Milos is Great!", ASCIIArtGenerator.ART_SIZE_MEDIUM, ASCIIArtGenerator.ASCIIArtFont.ART_FONT_DIALOG,"@");
+        System.out.println();
     }
+
+
 }
